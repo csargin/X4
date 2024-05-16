e72808389c8a8ac0b017db21ae1cecc6ea147191
csargin <120469159+csargin@users.noreply.github.com>
2024-05-16 13:56:29 +0300

    Update views.py

--- example/views.py
+++ example/views.py
@@ -6,3 +6,9 @@ from django.shortcuts import render, redirect
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def calendar(request):
+    return render(request, 'calendar.html')
+
