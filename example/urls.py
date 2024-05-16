34486ed45f3cbd1cad72fbb4da38de992d3d2a2c
csargin <120469159+csargin@users.noreply.github.com>
2024-05-16 13:45:58 +0300

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,7 @@
 # example/urls.py
 from django.urls import path
-from x4 import views
+from example import views
 from example.views import home
-from x4.example.views import home
-
 
 urlpatterns = [
     path('', views.home, name ="home"),
