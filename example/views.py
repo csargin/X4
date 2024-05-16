079ac6a5b3e4be78103452697fbfb02de562b1f1
csargin <120469159+csargin@users.noreply.github.com>
2024-05-16 14:10:34 +0300

    Update views.py

--- example/views.py
+++ example/views.py
@@ -12,3 +12,23 @@ def about(request):
 def calendar(request):
     return render(request, 'calendar.html' , {})
 
+def search(request):
+
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+
+        try:
+            try:
+                temp = si.get_quote_table(ticker, dict_result=False).set_index('attribute')
+                api= pd.DataFrame(data=temp)
+            except:
+                ticker = ticker + ".IS"
+                temp = si.get_quote_table(ticker, dict_result=False).set_index('attribute')
+                api= pd.DataFrame(data=temp)
+
+        except Exception as e:
+            api = "Error"
+        return render(request, 'search.html',{'api': api, 'ticker': ticker.upper()  })
+    else:
+        return render(request, 'search.html',{'ticker': "Enter a ticker symbol" })
+
