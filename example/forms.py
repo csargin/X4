f3bf508bf94382f08dc694223e6b0dafe7d02205
csargin <120469159+csargin@users.noreply.github.com>
2024-05-16 14:15:15 +0300

    Update forms.py

--- example/forms.py
+++ example/forms.py
@@ -1,1 +1,7 @@
+from django import forms
+from .models import example
 
+class StockForm(forms.ModelForm):
+    class Meta:
+        model = example
+        fields = ['ticker'] # fields = ['pub_date', 'headline', 'content', 'reporter']
