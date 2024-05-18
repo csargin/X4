37f050ac0bfc739c221f43bbc19c9ea70c78a4b2
csargin <120469159+csargin@users.noreply.github.com>
2024-05-18 11:03:10 +0300

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'api.wsgi.app'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DB_NAME"),
+        'NAME': os.environ.get("POSTGRES_DATABASE"),
         'USER': os.environ.get("POSTGRES_USER"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
         'HOST': os.environ.get("POSTGRES_HOST"),
