e4ea156857409cee61f71b6e707344c493c0ad72
csargin <120469159+csargin@users.noreply.github.com>
2024-05-28 15:51:40 +0300

    Update views.py

--- example/views.py
+++ example/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render, redirect
 from .models import Ships
 
 def home(request):
-    ticker_list = tuple(Ships.objects.values_list('id', flat = True))
+    #ticker_list = tuple(Ships.objects.values_list('id', flat = True))
+    ticker_list = Ships.objects.values_list('ship_name', 'ship_class', 'ship_race', 'ship_price', 'ship_weapon', 'ship_turret', 'ship_hull', 'ship_cargo', 'ship_dock', 'ship_hangar', 'ship_dlc', 'ship_role', 'ship_shield', 'ship_speed', 'id')
     if len(ticker_list)>0:
         try:
             api = ticker_list
